a58a53899747c8aee4d72d672696f937d69b7325
francis1024 <yiyboy@qq.com>
2018-09-09 00:07:47 +0800

    完成切换

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
 $(function() {
   init();
   function init() {
+    // 第一张图片功能
+    ImgTwo();
+    // 轮播图功能
+    scrollBannerImg();
+  }
+  // 第二张图片功能点
+  function ImgTwo() {
+    // 遍历所有img
+    $(".centreShowUl >li").on("click", function() {
+      console.log(123);
+      $(this)
+        .css("opacity", "1")
+        .siblings()
+        .css("opacity", "0.3");
+      $(".entreImg >img")
+        .eq($(this).index())
+        .css("display", "block")
+        .siblings()
+        .css("display", "none");
+    });
+    // 第一张图片鼠标移入显示
+    $(".centreShowUl").on("mouseover", function() {
+      $(".centreShowUl >li>img").css({"transform": "translateY(0%)","border-bottom":"0"});
+    });
+    // 第一张图片鼠标移开隐藏
+    $(".centreShowUl").on("mouseleave", function() {
+      $(".centreShowUl >li>img").css({"transform": "translateY(-99%)","border-bottom":"1px solid #000"});
+    });
+  }
+  // 轮播图功能
+  function scrollBannerImg() {
     $(".leftBtn").on("mouseover", function() {
       $(".ShowImgThreeBox").css("transform", "matrix(1, 0, 0, 1, 90, 0)");
       $(".leftBtn>span>div").css("width", "30px");
@@ -18,26 +49,23 @@ $(function() {
       $(".rightBtn>span>div").css("width", "0px");
     });
     $(".ShowImgThreeUl").on("mouseover", function() {
-        $(".ShowImgThreeUl >li>img").css('transform','translateY(0%)')
-        // transform: translateY(-99%);
-
+      $(".ShowImgThreeUl >li>img").css("transform", "translateY(0%)");
+      // transform: translateY(-99%);
     });
     $(".ShowImgThreeUl").on("mouseleave", function() {
-        $(".ShowImgThreeUl >li>img").css('transform','translateY(-99%)')
-
+      $(".ShowImgThreeUl >li>img").css("transform", "translateY(-99%)");
     });
     $("#carousel-example-generic").on("slide.bs.carousel", function(event) {
-        var $hoder = $("#carousel-example-generic").find(".item"),
-          $items = $(event.relatedTarget);
-        //getIndex就是轮播到当前位置的索引
-        var getIndex = $hoder.index($items);
-        console.log(getIndex);
-        $(".ShowImgThreeUl >li ")
-          .eq(getIndex)
-          .css("opacity", "1")
-          .siblings()
-          .css("opacity", ".3");
-      });
+      var $hoder = $("#carousel-example-generic").find(".item"),
+        $items = $(event.relatedTarget);
+      //getIndex就是轮播到当前位置的索引
+      var getIndex = $hoder.index($items);
+      console.log(getIndex);
+      $(".ShowImgThreeUl >li ")
+        .eq(getIndex)
+        .css("opacity", "1")
+        .siblings()
+        .css("opacity", ".3");
+    });
   }
-  
 });
